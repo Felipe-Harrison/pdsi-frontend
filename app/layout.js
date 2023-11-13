2aa2d4f0dc73e768e9ee8ebe9cf9d26d29f477f7
Felipe-Harrison <harrisufelipe@gmail.com>
2023-11-13 11:20:42 -0300

    fix: metadata

--- app/layout.js
+++ app/layout.js
@@ -10,10 +10,10 @@ import {NextUIProvider} from "@nextui-org/react";
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata = {
-  title: 'Chef Virtual',
-  description: 'O Chef Virtual é um sistema culinário inovador projetado para auxiliar cozinheiros, sejam eles amadores ou profissionais. Nosso Chef Virtual se adapta às suas necessidades e dúvidas, tornando sua jornada culinária mais deliciosa e eficiente. Faça perguntas sobre tópicos culinários, compartilhe com seus amigos, salve as suas melhores receitas.',
-}
+// export const metadata = {
+//   title: 'Chef Virtual',
+//   description: 'O Chef Virtual é um sistema culinário inovador projetado para auxiliar cozinheiros, sejam eles amadores ou profissionais. Nosso Chef Virtual se adapta às suas necessidades e dúvidas, tornando sua jornada culinária mais deliciosa e eficiente. Faça perguntas sobre tópicos culinários, compartilhe com seus amigos, salve as suas melhores receitas.',
+// }
 
 export default function RootLayout({ children }) {
   return (
